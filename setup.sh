d49fab9ca57aca48cab0c812141dac6040f72e8d
Brunno dos Santos <brunno@brunno.me>
2023-06-20 11:20:29 -0300

    Add setup.sh with homebrew installation

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/user/bin/env bash
+
+set -eou pipefail
+
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
