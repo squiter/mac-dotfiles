d9a51ab53c7280931a5fadd26ddc5230831102d4
Brunno dos Santos <brunno.dossantosleite@remote.com>
2025-03-20 15:56:37 -0300

    [fish] Adding `remote.config.fish`

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -25,6 +25,9 @@ if status is-interactive
     # Ensure gpg-agent was running
     eval "$(gpg-agent --daemon &>/dev/null)"
 
+    # Set my key as default
+    set -x GPGKEY A86BDBC5
+
     # Mise
     eval "$(/opt/homebrew/bin/mise activate fish)"
 
@@ -43,9 +46,6 @@ if status is-interactive
 
     # Used for ElixirLS
     fish_add_path -aP $HOME/.local/bin
-
-    # Set my key as default
-    set -x GPGKEY A86BDBC5
 
     # FZF Configs
     set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border'
@@ -71,16 +71,9 @@ if status is-interactive
         MIX_ENV=$env mix ecto.migrate
     end
 
-    # Remote aliases
-    abbr -a neo-nld-tests "MIX_ENV=test mix do --app netherlands test"
-    abbr -a neo-nld-e2e "MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/netherlands"
-    abbr -a neo-nld-api-tests "mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/netherlands"
-    abbr -a neo-nld-all-tests "MIX_ENV=test mix do --app netherlands test && MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/netherlands && mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/netherlands"
+    source_if_exists $HOME/.config/fish/remote.config.fish
 
-    abbr -a neo-ca-tests "MIX_ENV=test mix do --app canada test"
-    abbr -a neo-ca-e2e "MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/canada"
-    abbr -a neo-ca-api-tests "mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/canada"
-    abbr -a neo-ca-all-tests "MIX_ENV=test mix do --app canada test && MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/canada && mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/canada"
+    # Remote aliases
 
     # Keybinds
     function fish_user_key_bindings
---
+++ home/.config/fish/remote.config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    # AWS Config
+    set AWS_DEFAULT_PROFILE sts
+
+    if not command -q remotectl
+        echo "remotectl not found! Installing..."
+        mise plugin add remotectl git@gitlab.com:remote-com/devops/asdf-remotectl.git
+        mise use remotectl@latest --global
+    end
+
+    if not test -f $fish_complete_path[1]/remotectl.fish
+        echo "remotectl autocomplete file not found! Generating..."
+        remotectl completion fish > $fish_complete_path[1]/remotectl.fish
+    end
+    
+    # Aliases
+    abbr -a neo-nld-tests "MIX_ENV=test mix do --app netherlands test"
+    abbr -a neo-nld-e2e "MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/netherlands"
+    abbr -a neo-nld-api-tests "mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/netherlands"
+    abbr -a neo-nld-all-tests "MIX_ENV=test mix do --app netherlands test && MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/netherlands && mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/netherlands"
+
+    abbr -a neo-ca-tests "MIX_ENV=test mix do --app canada test"
+    abbr -a neo-ca-e2e "MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/canada"
+    abbr -a neo-ca-api-tests "mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/canada"
+    abbr -a neo-ca-all-tests "MIX_ENV=test mix do --app canada test && MIX_ENV=e2e mix test.e2e apps/neo_web/test/e2e/canada && mix test apps/neo_web/test/unit/neo_web/controllers/api/v1/canada"
+end
