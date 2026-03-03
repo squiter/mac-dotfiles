d0debce56c2330e68fcc94ba0f4d80d9d624aabe
Brunno dos Santos <brunno.dossantosleite@remote.com>
2026-03-03 15:43:18 -0300

    [jj] Adding jj to the vcs function used by fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,17 @@
 if status is-interactive
+    function fish_vcs_prompt --description "Print all vcs prompts"
+        # If a prompt succeeded, we assume that it's printed the correct info.
+        # This is so we don't try svn if git already worked.
+        fish_jj_prompt $argv
+        or fish_git_prompt $argv
+        or fish_hg_prompt $argv
+        or fish_fossil_prompt $argv
+        # The svn prompt is disabled by default because it's quite slow on common svn repositories.
+        # To enable it uncomment it.
+        # You can also only use it in specific directories by checking $PWD.
+        # or fish_svn_prompt
+    end
+    
     set -x cmd_notification_threshold 15000
 
     # Notify when long command finished
@@ -38,7 +51,7 @@ if status is-interactive
 
         __d12_prompt__check_duration
 
-        string join '' -- $stat ' ' (set_color green) '[📁 ' (prompt_pwd)']' (set_color normal) (fish_git_prompt) '> '
+        string join '' -- $stat ' ' (set_color green) '[📁 ' (prompt_pwd)']' (set_color normal) (fish_vcs_prompt) '> '
     end
 
     function fzf_history
